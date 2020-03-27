89ba9c482afc4ab0617afbc1491ae4cd6536471e
Justin-Vuong <vuong.justin01@gmail.com>
2020-03-26 22:48:56 -0400

    March 26, 2019
    Implemented upload feature to Youtube when there is motion

--- MotionDetection.py
+++ MotionDetection.py
@@ -1,8 +1,10 @@
 import cv2
 import datetime
 import time
+import datetime
 import imutils
 from imutils.video import VideoStream
+import UploadVid
 
 class FrameBuffer:
     def __init__(self):
@@ -31,98 +33,107 @@ class VidBuffer:
             currentLoop.next = nextLoop
             currentLoop = nextLoop
         currentLoop.next = self.head
+
+def startCamera(yt):        
+    RefFrameCount = 0
+
+    vs = VideoStream(src=0).start()
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+
+    #For Recording
+    bufferLength = 5
+    fps = 20
+    vidLoop = VidBuffer(bufferLength,fps)
+    saveImg = vidLoop.head
+    videoIdentifier = 0
+    loopback = -1
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    isRecording = False
+    time.sleep(2)
+
+    while True:
+        print("hi")
+        frame = vs.read()
+        text = "Nobody"
+        if frame is None:
+            print("Can't read form camera")
+            break
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21,21), 0)
+        
+        if RefFrameCount % 250 == 0:
+            firstFrame = gray
+        RefFrameCount+= 1   
+        
+        frameDelta = cv2.absdiff(firstFrame,gray)
+        thresh = cv2.threshold(frameDelta, 25,255,cv2.THRESH_BINARY)[1]
+        
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
         
         
-RefFrameCount = 0
-
-vs = VideoStream(src=0).start()
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-
-#For Recording
-bufferLength = 5
-fps = 20
-vidLoop = VidBuffer(bufferLength,fps)
-saveImg = vidLoop.head
-videoIdentifier = 0
-loopback = -1
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-isRecording = False
-time.sleep(2)
-
-while True:
-    frame = vs.read()
-    text = "Nobody"
-    if frame is None:
-        break
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21,21), 0)
-    
-    if RefFrameCount % 250 == 0:
-        firstFrame = gray
-    RefFrameCount+= 1   
-    
-    frameDelta = cv2.absdiff(firstFrame,gray)
-    thresh = cv2.threshold(frameDelta, 25,255,cv2.THRESH_BINARY)[1]
-    
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    
-    
-    for c in cnts:
-        if cv2.contourArea(c) < 250:
-            continue
-        (x,y,w,h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
-        text = "Motion!"
-   
-    if len(cnts) > 0:
-        if loopback == -1:
-            videoIdentifier += 1
-            vidClip = cv2.VideoWriter('SavedClips/Footage' + str(videoIdentifier) + '.avi', fourcc, 20.0, (640, 480))  
-        isRecording = True
-        loopback = ((bufferLength*fps)+(int(saveImg.num)-1)) % (bufferLength*fps)
-         
-    cv2.putText(frame, "Room Status {}: {}".format(RefFrameCount,text), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,255), 1)
-    
-    cv2.imshow("Security Feed", frame)
-    cv2.imshow("Thresh", thresh)
-    cv2.imshow("Frame Delta", frameDelta)
-    key = cv2.waitKey(1) & 0xFF
-      
-    print("On num:" + str(saveImg.num) + " and isRecording is " + str(isRecording) + " Loopback is: " + str(loopback))
-    
-    if isRecording or loopback != -1:
-        if saveImg.num == loopback:
-            #Have not detected motion
-            if isRecording:
-                isRecording = False
-            else:
-                loopback = -1
+        for c in cnts:
+            if cv2.contourArea(c) < 250:
+                continue
+            (x,y,w,h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+            text = "Motion!"
+       
+        if len(cnts) > 0:
+            if loopback == -1:
+                name_time = datetime.datetime.now().strftime("%H:%M_%A_%B_%d_%Y")
+                vidClip = cv2.VideoWriter("SavedClips/" + name_time + ".avi", fourcc, 20.0, (640, 480))
+            isRecording = True
+            loopback = ((bufferLength*fps)+(int(saveImg.num)-1)) % (bufferLength*fps)
+             
+        cv2.putText(frame, "Room Status {}: {}".format(RefFrameCount,text), (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),2)
+        cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,255), 1)
         
-        #Just detected motion, continue to record
+        cv2.imshow("Security Feed", frame)
+        #cv2.imshow("Thresh", thresh)
+        #cv2.imshow("Frame Delta", frameDelta)
+        key = cv2.waitKey(1) & 0xFF
+          
+        #print("On num:" + str(saveImg.num) + " and isRecording is " + str(isRecording) + " Loopback is: " + str(loopback))
+        
+        if isRecording or loopback != -1:
+            if saveImg.num == loopback:
+                #Have not detected motion
+                if isRecording:
+                    isRecording = False
+                else:
+                    loopback = -1
+                    
+                    #Upload Video
+                    settings = {}
+                    settings["file"] = "/home/pi/Desktop/Video-Doorbell/SavedClips/" + name_time + ".avi"
+                    settings["title"] = name_time
+                    settings["description"] = "Video Capture from " + name_time
+                    UploadVid.youtube_login(yt, settings)
             
-        if saveImg.clip is not None:
-            vidClip.write(saveImg.clip)
-            print("Write")
-    
-    if cv2.waitKey(1) & 0xFF == ord('r'):
-        if loopback == -1:
-            videoIdentifier += 1
-            vidClip = cv2.VideoWriter('Footage' + str(videoIdentifier) + '.avi', fourcc, 20.0, (640, 480))  
-        isRecording = True
-        loopback = ((bufferLength*fps)+(int(saveImg.num)-1)) % (bufferLength*fps)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            #Just detected motion, continue to record
+                
+            if saveImg.clip is not None:
+                vidClip.write(saveImg.clip)
+                print("Write")
         
-    saveImg.clip = frame
-    saveImg = saveImg.next
-    
-    if key == ord("q"):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('r'):
+            if loopback == -1:
+                name_time = datetime.datetime.now().strftime("%H:%M_%A_%B_%d_%Y")
+                vidClip = cv2.VideoWriter("SavedClips/" + name_time + ".avi", fourcc, 20.0, (640, 480))
+            isRecording = True
+            loopback = ((bufferLength*fps)+(int(saveImg.num)-1)) % (bufferLength*fps)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+        saveImg.clip = frame
+        saveImg = saveImg.next
+        
+        if key == ord("q"):
+            break
 
-vs.stop()
-cv2.destroyAllWindows()
+    vs.stop()
+    cv2.destroyAllWindows()
 
---
+++ Oauth.py
@@ -0,0 +1,64 @@
+import webbrowser
+import socket
+import sys
+import requests
+import json
+import requests
+import gdata.youtube
+import gdata.youtube.service
+def getAuth():
+    HOST = ''   # Symbolic name, meaning all available interfaces
+    PORT = 55555 # Arbitrary non-privileged port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #Bind socket to local host and port
+    try:
+        s.bind((HOST, PORT))
+    except socket.error as msg:
+        print('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+        sys.exit()
+    #Start listening on socket
+    s.listen(1)
+    
+    #Open link in browser
+    webbrowser.open_new("https://accounts.google.com/o/oauth2/auth?client_id=223289540342-j8o4l4p6meo9pbnq1ju3dq8jv6o278qa.apps.googleusercontent.com&redirect_uri=http://localhost:55555&response_type=code&scope=https://www.googleapis.com/auth/youtube.upload")
+
+    conn, addr = s.accept()
+    print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+    data = conn.recv(1024)
+    data = data.decode("utf-8")
+    data = data.split("&")[0]
+    start = data.find("code=")
+    data = data[start+5:]
+
+    authDict = parseApiData(data)
+
+    r = requests.post("https://oauth2.googleapis.com/token", authDict)
+
+    conn.close()
+    s.close()
+    
+    refreshReq = r.text.split(",")
+    authToken = refreshReq[0][21:len(refreshReq[0])-1]
+    print("auth-> " + str(authToken))
+
+    refresh_token = refreshReq[2][21:len(refreshReq[2])-1]
+    print("refresh->" + str(refresh_token))
+    
+    tokens = {"token":str(authToken), "refresh_token":str(refresh_token)}
+    
+    return tokens
+
+def parseApiData(data = None):
+    apiAuth = open("/home/pi/Desktop/Video-Doorbell/client_secret.json", "r")
+    apiAuth = apiAuth.read()
+    apiAuth = json.loads(apiAuth)
+
+    authReqDict = {}
+    if data != None:
+        authReqDict["code"] = data 
+    authReqDict["client_id"] = apiAuth["client_id"]
+    authReqDict["client_secret"] = apiAuth["client_secret"]
+    authReqDict["redirect_uri"] = "http://localhost:55555"
+    authReqDict["grant_type"] = "authorization_code"
+    authReqDict["response_type"] = "code"
+    return authReqDict
